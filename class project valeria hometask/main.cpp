3fa7b1e871a3d372962687210f1c3d69d381c6b3
valeriabukova <valeriasergeevna688@gmail.com>
2024-09-17 12:37:31 +0300

    Initial Commit

---
+++ class project valeria hometask/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  class project valeria hometask
+//
+//  Created by Valeria  Bukova on 17.09.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
